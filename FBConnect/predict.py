408c573c85c9c3194ef09baccd5466b479bac240
Jacob Sebastian <jasebs94@gmail.com>
2020-05-13 12:29:27 +0530

    first project

--- FBConnect/predict.py
+++ FBConnect/predict.py
@@ -21,4 +21,11 @@ def getIntent(input):
 
     r = requests.get(f'https://westus.api.cognitive.microsoft.com/luis/prediction/v3.0/apps/{appId}/slots/production/predict',headers=headers, params=params)
     print(r.json())
+    result = r.properties["luisResult"]
+    print(str(result.intents[0]))
+    intentDetails = json.loads((str(result.intents[0])).replace("'", "\""))
+    intent = intentDetails.get('intent')
+    score = intentDetails.get('score')
+    print(intent)
+    print(score)
 
